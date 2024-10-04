090877cf9b1b33aeb347691ceee240d8a72fac18
Zurihaa <tle33@hawk.iit.edu>
2024-10-04 18:32:42 -0400

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,16 +1,30 @@
-import random
-import string
+#pip install cryptodome
+import Crypto
+print(Crypto.__version__)
 
-def shiftcipher(text,n):
+
+def shiftcipher(text,key):
     ciphertext = ""
     for i in range(len(text)):
         ch = text[i]
         if ch == " ":
             ciphertext += " "
         elif ch.isupper():
-            ciphertext += chr((ord(ch) + n-65) % 26 + 65)        
+            ciphertext += chr((ord(ch) + key - 65) % 26 + 65)        
         else:
-            ciphertext += chr((ord(ch) + n-97) % 26 + 97)
+            ciphertext += chr((ord(ch) + key - 97) % 26 + 97)
+    return ciphertext
+
+def shift_decryption(text, key):
+    ciphertext = ""
+    for i in range(len(text)):
+        ch = text[i]
+        if ch == " ":
+            ciphertext += " "
+        elif ch.isupper():
+            ciphertext += chr((ord(ch) - key - 65) % 26 + 65)        
+        else:
+            ciphertext += chr((ord(ch) - key - 97) % 26 + 97)
     return ciphertext
 
 def permutation_alphabet():
@@ -21,6 +35,9 @@ def permutation_alphabet():
     for i in range(26):
         key[lowerAlphabet[i]] = lowerAlphabet[alphabet[i]]
     return key
+
+def reverse_key(key):
+    return {i: j for j, i in key.items()}
 
 def permutationcipher(text, key):
     ciphertext = ""
@@ -34,15 +51,28 @@ def permutationcipher(text, key):
             ciphertext += permuted_char.upper() if uppercase else permuted_char
     return ciphertext
 
+def permutationdecrypt(text, key):
+    ciphertext = ""
+    reverse = reverse_key(key)
+    for i in text:
+        if i == " ":
+            ciphertext += " "
+        else:
+            uppercase = i.isupper()
+            ch = i.lower()
+            og_char = reverse[ch]
+            ciphertext += og_char.upper() if uppercase else og_char
+    return ciphertext
+
 def simpletranspositioncipher(text, row, column):
     
-    text = text.replace(" ", "x")
+    text = text.replace(" ", "X")
 
     total = row * column
     fill = total - len(text)
 
     if fill > 0:
-        text += "x" * fill
+        text += "X" * fill
 
     matrix = []
 
@@ -65,7 +95,30 @@ def simpletranspositioncipher(text, row, column):
 
     return ciphertext
 
-def doubletranspositioncipher(text, row, column, row_pattern, col_pattern):
+def simpletranspositiondecrypt(text, row, column):
+    matrix = []
+    
+    for i in range(row):
+        matrix = matrix + [[]]
+        for j in range(column):
+            matrix[i] = matrix[i] + [' ']
+
+    k = 0
+
+    for i in range(column):
+        for j in range(row):
+            matrix[j][i] = text[k]
+            k += 1
+
+    ciphertext = ""
+
+    for i in range(row):
+        for j in range(column):
+            ciphertext += matrix [i][j]
+
+    return ciphertext
+
+def doubletranspositioncipher(text, row, column, row_pattern:list, col_pattern:list):
     
     text = text.replace(" ", "x")
 
@@ -73,7 +126,7 @@ def doubletranspositioncipher(text, row, column, row_pattern, col_pattern):
     fill = total - len(text)
 
     if fill > 0:
-        text += "x" * fill
+        text += "X" * fill
 
     matrix = []
 
@@ -84,8 +137,8 @@ def doubletranspositioncipher(text, row, column, row_pattern, col_pattern):
 
     k = 0
 
-    for i in range(column):
-        for j in range(row):
+    for i in range(row):
+        for j in range(column):
             matrix[i][j] = text[k]
             k += 1
 
@@ -111,9 +164,43 @@ def doubletranspositioncipher(text, row, column, row_pattern, col_pattern):
 
     return ciphertext
 
-#Reference from https://www.geeksforgeeks.org/vigenere-cipher
+def doubletranspositiondecrypt(text, row, column, row_pattern:list, col_pattern:list):
+    decrypt_matrix = []
+
+    for i in range(row):
+        decrypt_matrix = decrypt_matrix + [[]]
+        for j in range(column):
+            decrypt_matrix[i] = decrypt_matrix[i] + [' ']
+
+    k = 0
+    for i in range(row):
+        for j in range(column):
+            decrypt_matrix[i][j] = text[k]
+            k += 1
+
+    og_matrix = []
+    for i in range(row):
+        og_matrix = og_matrix + [[]]
+        for j in range(column):
+            og_matrix[i] = og_matrix[i] + [' ']
+    
+    for i in range(row):
+        for j, col_pos in enumerate(col_pattern):
+            og_matrix[i][col_pos - 1] = decrypt_matrix[i][j]
+
+    final_matrix = []
+    for i, row_pos in enumerate(row_pattern):
+        final_matrix.append(og_matrix[row_pos - 1])
+
+    ciphertext = ""
+    for i in range(row):
+        for j in range(column):
+            ciphertext += final_matrix[i][j]
+
+    return ciphertext
+
 def vigenerecipher(text, key):
-    
+
     text_key = (key * (len(text) // len(key))) + key[:len(text) % len(key)]
 
     ciphertext = ""
@@ -129,29 +216,250 @@ def vigenerecipher(text, key):
     
     return ciphertext
 
-def vigeneredecrypht(text, key):
+def vigeneredecrypt(text, key):
     text_key = (key * (len(text) // len(key))) + key[:len(text) % len(key)]
 
-    ciphertext = ""
+    decrypttext = ""
 
     for i in range(len(text)):
         ch = text[i]
         if ch.isupper():
-            ciphertext += chr((ord(ch) - ord(text_key[i]) + 26) % 26 + ord("A"))
+            decrypttext += chr((ord(ch) - ord(text_key[i]) + 26) % 26 + ord("A"))
         elif ch.islower():
-            ciphertext += chr((ord(ch) - ord(text_key[i]) + 26) % 26 + ord("A"))
+            decrypttext += chr((ord(ch) - ord(text_key[i]) + 26) % 26 + ord("A"))
         else:
-            ciphertext += ch
+            decrypttext += ch
     
-    return ciphertext
+    return decrypttext
 
 
-print("Choose your encryption mode:")
-print("1. Shift Cipher")
-print("2. Permutation Cipher")
 
-text = input("Plain Text: ")
-#mode = input("Choose your encryption mode: ")
-#row = int(input())
-#col = int(input())
-print(vigeneredecrypht(text, "VIG"))
+def main():
+    while True:
+        try:
+            choice = int(input("Do you want to encrypt or decrypt (1 for encrypt/2 for decrypt) ?"))
+            if choice not in [1, 2]:
+                raise ValueError("Choice must be 1 or 2.")
+            break
+        except ValueError as e:
+            print("Invalid input. Please enter 1 or 2.")
+  
+    while True:
+        try:
+            plaintext = input("Enter plaintext: ")
+            if plaintext == "": 
+                raise ValueError("Plaintext cannot be empty.")
+            break
+        except ValueError as e:
+            print("Invalid input. Please enter an text for plaintext.")
+  
+    if choice == 1:
+        print("Choose your encryption mode:")
+        print("1. Shift")
+        print("2. Permutation")
+        print("3. Simple Transposition")
+        print("4. Double Transposition")
+        print("5. Vigenere")
+        print("6. AES")
+        print("7. DES")
+        print("8. 3DES")
+
+        while True:
+            try:
+                option = int(input("Enter from 1 to 8: "))
+                if choice not in [1, 2, 3, 4, 5, 6, 7, 8]:
+                    raise ValueError("Choice must be from 1 to 8.")
+                break
+            except ValueError as e:
+                print("Invalid input. Choice must be from 1 to 8.")
+
+        if option == 1:
+            while True:
+                try:
+                    n = int(input("Enter key: "))
+                    if n == 0 or n >26: 
+                        raise ValueError("Key cannot be 0.")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Please enter an int for key.")
+            print("Ciphertext: ", shiftcipher(plaintext, n))
+        elif option == 2:
+            while True:
+                try:
+                    key = dict(input("Enter your key: "))
+                    if key == None:
+                        key = permutation_alphabet()
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            print("Ciphertext: ", permutationcipher(plaintext, key))
+        elif option == 3:
+            while True:
+                try:
+                    row = int(input("Enter number of rows: "))
+                    if row is not int:
+                        raise ValueError("Must be an interger")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            while True:
+                try:
+                    column = int(input("Enter number of rows: "))
+                    if column is not int:
+                        raise ValueError("Must be an interger")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            print("CipherText: ", simpletranspositioncipher(plaintext, row, column))
+        elif option == 4:
+            while True:
+                try:
+                    row = int(input("Enter number of rows: "))
+                    if row is not int:
+                        raise ValueError("Must be an interger")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            while True:
+                try:
+                    column = int(input("Enter number of rows: "))
+                    if column is not int:
+                        raise ValueError("Must be an interger")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            while True:
+                try:
+                    row_pattern = list[int](input("Enter row pattern: "))
+                    if row_pattern is not list[int] or len(row_pattern) != row:
+                        raise ValueError("Try Again")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            while True:
+                try:
+                    col_pattern = list[int](input("Enter col pattern: "))
+                    if col_pattern is not list[int] or len(col_pattern) != row:
+                        raise ValueError("Try Again")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            print("CipherText: ", doubletranspositioncipher(plaintext, row, column, row_pattern, col_pattern))
+        elif option == 5:
+            while True:
+                try:
+                    key = str(input("Enter your key: "))
+                    if row is not int:
+                        raise ValueError("Try Again")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            print(vigenerecipher(plaintext, key))
+        elif option == 6:
+            return 1
+        elif option == 7:
+            return 1
+        elif option == 8:
+            return 1
+        
+    elif choice == 2:
+        print("Choose your decryption mode:")
+        print("1. Shift")
+        print("2. Permutation")
+        print("3. Simple Transposition")
+        print("4. Double Transposition")
+        print("5. Vigenere")
+        print("6. AES")
+        print("7. DES")
+        print("8. 3DES")
+
+        if option == 1:
+            while True:
+                try:
+                    n = int(input("Enter key: "))
+                    if n == 0 or n >26: 
+                        raise ValueError("Key cannot be 0.")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Please enter an int for key.")
+            print("Ciphertext: ", shift_decryption(plaintext, n))
+        elif option == 2:
+            while True:
+                try:
+                    key = dict(input("Enter your key: "))
+                    if key == None:
+                        key = permutation_alphabet()
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            print("Ciphertext: ", permutationdecrypt(plaintext, key))
+        elif option == 3:
+            while True:
+                try:
+                    row = int(input("Enter number of rows: "))
+                    if row is not int:
+                        raise ValueError("Must be an interger")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            while True:
+                try:
+                    column = int(input("Enter number of rows: "))
+                    if column is not int:
+                        raise ValueError("Must be an interger")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            print("CipherText: ", simpletranspositiondecrypt(plaintext, row, column))
+        elif option == 4:
+            while True:
+                try:
+                    row = int(input("Enter number of rows: "))
+                    if row is not int:
+                        raise ValueError("Must be an interger")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            while True:
+                try:
+                    column = int(input("Enter number of rows: "))
+                    if column is not int:
+                        raise ValueError("Must be an interger")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            while True:
+                try:
+                    row_pattern = list[int](input("Enter row pattern: "))
+                    if row_pattern is not list[int] or len(row_pattern) != row:
+                        raise ValueError("Try Again")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            while True:
+                try:
+                    col_pattern = list[int](input("Enter col pattern: "))
+                    if col_pattern is not list[int] or len(col_pattern) != row:
+                        raise ValueError("Try Again")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            print("CipherText: ", doubletranspositiondecrypt(plaintext, row, column, row_pattern, col_pattern))
+        elif option == 5:
+            while True:
+                try:
+                    key = str(input("Enter your key: "))
+                    if row is not int:
+                        raise ValueError("Try Again")
+                    break
+                except ValueError as e:
+                    print("Invalid input. Try Again.")
+            print(vigeneredecrypt(plaintext, key))
+        elif option == 6:
+            return 1
+        elif option == 7:
+            return 1
+        elif option == 8:
+            return 1
+
+main()
